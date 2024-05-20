191b909a161821ff7f43be448cebdbf03b2be4e8
studentq <studentq.work@yandex.ru>
2024-05-20 17:46:31 +0400

    first commit

---
+++ app/main.py
@@ -0,0 +1,9 @@
+import uvicorn
+from fastapi import FastAPI
+from .api_v1 import router as router_v1
+
+app = FastAPI()
+app.include_router(router=router_v1, prefix="/api/v1")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
